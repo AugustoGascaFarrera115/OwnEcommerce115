b9869690fd61150d42baf2ede1e26a0f961017d4
Saul Augusto Gasca Farrera <saulagf115@gmail.com>
2020-08-24 14:40:27 -0500

    New Migration Entities and Configurations Migrations Added

--- SaulShop115/Infrastructure/Data/StoreContext.cs
+++ SaulShop115/Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -15,5 +16,20 @@ namespace Infrastructure.Data
         //Products will be the products table
         //DbSet<Product> take the entiti named product
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+
+        //this method OnModelCreating() is the responsible to create the mihrations to DB
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
     }
 }
