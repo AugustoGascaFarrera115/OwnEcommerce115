0690cc8a93bd392513ab981b5a3f13895a8c1093
Saul Augusto Gasca Farrera <saulagf115@gmail.com>
2020-10-06 17:22:26 -0500

    Font Awesome Css Bug Fixed

--- SaulShop115/client/src/app/app.module.ts
+++ SaulShop115/client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
